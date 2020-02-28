835bbbb45f2768794b5beac62dde9bfb18b01c75
Parul-TTN <parul@tothenew.com>
2020-02-28 22:06:38 +0530

    Assignmnet Junit

--- src/test/java/com/im/FirstTest.java
+++ src/test/java/com/im/FirstTest.java
@@ -6,8 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstTest {
     @Test
-    void canary(){
-        assertTrue(true);
+    void should_return_true_when_pallindrome()
+    {
+        //given
+        String myStr = "madam";
+        //when
+        boolean res = new First().isPallindrome(myStr);
+        //then
+        assertTrue(res);
+    }
+    @Test
+    void should_return_false_when_notpallindrome(){
+        //given
+        String myStr2  = "hello";
+        //when
+        boolean res = new First().isPallindrome(myStr2);
+        //then
+        assertFalse(res);
     }
 
 }
--- src/test/java/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/healthycoderapp/BMICalculatorTest.java
@@ -1,14 +1,33 @@
 package healthycoderapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
 
-    @Test
-    void fixMe(){
-        assertTrue(true);
-    }
+//    @Test
+//    void should_return_true_when_DietRecommended() {
+//        //given
+//        double weigth = 89.0;
+//        double height = 1.7;
+//        //when
+//        boolean recommended = BMICalculator.isDietRecommended(weigth, height);
+//        //then
+//        assertTrue(recommended);
+//    }
+//
+//    @Test
+//    void should_ThrowArithmeticException_when_DietRecommended(){
+//        //given
+//        double weigth = 89.0;
+//        double height = 0.0;
+//        //when
+//        Executable executable = () -> BMICalculator.isDietRecommended(weigth,height);
+//        //then
+//        assertThrows(ArithmeticException.class, executable);
+//
+//    }
 
 }
