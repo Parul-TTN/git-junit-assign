4a142b425a4fc0e9793f3a784db33e9d65d9324e
Parul-TTN <parul@tothenew.com>
2020-03-02 15:50:43 +0530

    junit Assignment

--- src/test/java/com/im/FirstTest.java
+++ src/test/java/com/im/FirstTest.java
@@ -1,13 +1,24 @@
 package com.im;
 
+import healthycoderapp.BMICalculator;
+import healthycoderapp.Coder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstTest {
+
+
     @Test
-    void should_return_true_when_pallindrome()
-    {
+    void should_return_true_when_pallindrome() {
         //given
         String myStr = "madam";
         //when
@@ -15,14 +26,85 @@ public class FirstTest {
         //then
         assertTrue(res);
     }
+
     @Test
-    void should_return_false_when_notpallindrome(){
+    void should_return_false_when_notpallindrome() {
         //given
-        String myStr2  = "hello";
+        String myStr2 = "hello";
         //when
         boolean res = new First().isPallindrome(myStr2);
         //then
         assertFalse(res);
     }
+
+    @Test
+    void should_FilterEvenElements() {
+        //given
+        List<Integer> elements = new ArrayList<>();
+        elements.add(4);
+        elements.add(5);
+        elements.add(8);
+        elements.add(13);
+
+        List<Integer> expected = Arrays.asList(5, 13);
+
+        //when
+        List<Integer> evenElements = new First().filterEvenElements(elements);
+        //then
+        assertIterableEquals(expected, evenElements);
+
+    }
+
+    @Test
+    void should_calculateAverage_whenListNotEmpty() {
+        //given
+        List<BigDecimal> values = new ArrayList<>();
+        values.add(new BigDecimal(1));
+        values.add(new BigDecimal(7));
+        values.add(new BigDecimal(9));
+        values.add(new BigDecimal(10));
+
+        BigDecimal expAverage = new BigDecimal(6.75);
+        //when
+        BigDecimal Average = new First().calculateAverage(values);
+        //then
+        assertEquals(expAverage, Average);
+    }
+
+    @Test
+    void should_ThrowRunTimeException_when_ListEmpty() {
+        //given
+        List<BigDecimal> values = new ArrayList<>();
+
+        //when
+        Executable executable = () -> new First().calculateAverage(values);
+        //then
+        assertThrows(RuntimeException.class, executable);
+    }
+
+    @Test
+    void should_ThrowRunTimeException_when_ListIsNull() {
+        //given
+        List<BigDecimal> values = null;
+
+        //when
+        Executable executable = () -> new First().calculateAverage(values);
+        //then
+        assertThrows(RuntimeException.class, executable);
+    }
+    @Test
+    void should_return_string_after_replacement(){
+        //given
+        String mainStr = "HelloWorld";
+        String subStr = "oWo";
+        String replacedString = "abc";
+
+        String ExpectedStr = "Hellabcrld";
+        //when
+        String resultString = new First().replaceSubString(mainStr,subStr,replacedString);
+        //then
+        assertEquals(ExpectedStr,resultString);
+    }
+
 
 }
--- src/test/java/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/healthycoderapp/BMICalculatorTest.java
@@ -3,31 +3,74 @@ package healthycoderapp;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
 
-//    @Test
-//    void should_return_true_when_DietRecommended() {
-//        //given
-//        double weigth = 89.0;
-//        double height = 1.7;
-//        //when
-//        boolean recommended = BMICalculator.isDietRecommended(weigth, height);
-//        //then
-//        assertTrue(recommended);
-//    }
-//
-//    @Test
-//    void should_ThrowArithmeticException_when_DietRecommended(){
-//        //given
-//        double weigth = 89.0;
-//        double height = 0.0;
-//        //when
-//        Executable executable = () -> BMICalculator.isDietRecommended(weigth,height);
-//        //then
-//        assertThrows(ArithmeticException.class, executable);
-//
-//    }
+    @Test
+    void should_return_true_when_DietRecommended() {
+        //given
+        double weigth = 89.0;
+        double height = 1.7;
+        //when
+        boolean recommended = BMICalculator.isDietRecommended(weigth, height);
+        //then
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_ThrowArithmeticException_when_DietRecommended(){
+        //given
+        double weigth = 89.0;
+        double height = 0.0;
+        //when
+        Executable executable = () -> BMICalculator.isDietRecommended(weigth,height);
+        //then
+        assertThrows(ArithmeticException.class, executable);
+
+    }
+    @Test
+    void should_returncoderwithworstbmi_when_listnotempty(){
+        //given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80,68.8));
+        coders.add(new Coder(1.82,98.8));
+        coders.add(new Coder(1.82,64.7));
+
+        //when
+        Coder codersWorstBmi = BMICalculator.findCoderWithWorstBMI(coders);
+
+        //then
+        assertEquals(1.82,codersWorstBmi.getHeight());
+        assertEquals(98.8,codersWorstBmi.getWeight());
+    }
+    @Test
+    void should_returncoderwithworstbmi_when_listISempty(){
+        //given
+        List<Coder> coders = new ArrayList<>();
+
+
+        //when
+        Coder codersWorstBmi = BMICalculator.findCoderWithWorstBMI(coders);
+        //then
+        assertNull(codersWorstBmi);
+    }
+    @Test
+    void should_returnCorrectBmiScoreArray_WhenCoderListIsNotEmpty(){
+        //given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80,60.0));
+        coders.add(new Coder(1.82,98.0));
+        coders.add(new Coder(1.82,64.7));
+        double[] expected = {18.52,29.59,19.53};
+        //when
+        double[] bmiScores = BMICalculator.getBMIScores(coders);
+        //then
+        assertArrayEquals(expected,bmiScores);
+
+    }
 
 }
